98ba970121a8bdfe88949721af8f16ea54ed6ccf
Sagar Akuch <93991209+sagarakuch00@users.noreply.github.com>
2024-02-26 09:25:56 +0000

    New teing methods in prototype

---
+++ 01_basics/string.js
@@ -0,0 +1,21 @@
+const name = "sagarakuch"
+console.log(name);
+
+console.log(`wlcome to my repo ${50} i am glad for you ${"thanx" .toUpperCase}
+
+console.log(name charcode);
+
+
+// const newname = name.substring(0, 4)
+// console.log(newname);
+
+// const name1 = name.slice(-8 , 4)
+
+// console.log(name1);
+
+// const fullName = "  sagarakuch   "
+
+// console.log(fullName. trim());
+
+const game = new String("school")
+console.log(game);
